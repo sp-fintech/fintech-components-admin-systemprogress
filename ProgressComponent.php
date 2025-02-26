4ed6453d9dd0bb82d85f237fb425891cc7da88bf
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2025-02-26 17:01:30 +1100

    Initial Commit

---
+++ ProgressComponent.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Apps\Fintech\Components\System\Progress;
+
+use System\Base\BaseComponent;
+
+class ProgressComponent extends BaseComponent
+{
+    public function getProgressAction()
+    {
+        $this->requestIsPost();
+
+        $fileName = $this->session->getId();
+
+        if (isset($this->postData()['session'])) {
+            $fileName = $this->postData()['session'];
+        } else if (isset($this->postData()['file_name'])) {
+            $fileName = $this->postData()['file_name'];
+        }
+
+        $progress = $this->basepackages->progress->getProgress($fileName);
+
+        if ($progress) {
+            $this->addResponse(
+                $this->basepackages->progress->packagesData->responseMessage,
+                $this->basepackages->progress->packagesData->responseCode,
+                $progress,
+            );
+        } else {
+            $this->addResponse(
+                $this->basepackages->progress->packagesData->responseMessage,
+                $this->basepackages->progress->packagesData->responseCode
+            );
+        }
+    }
+
+    public function cancelProgressAction()
+    {
+        $this->requestIsPost();
+
+        $fileName = $this->session->getId();
+
+        if (isset($this->postData()['session'])) {
+            $fileName = $this->postData()['session'];
+        } else if (isset($this->postData()['file_name'])) {
+            $fileName = $this->postData()['file_name'];
+        }
+
+        $progress = $this->basepackages->progress->cancelProgress($fileName);
+
+        if ($progress) {
+            $this->addResponse(
+                $this->basepackages->progress->packagesData->responseMessage,
+                $this->basepackages->progress->packagesData->responseCode,
+                $progress,
+            );
+        } else {
+            $this->addResponse(
+                $this->basepackages->progress->packagesData->responseMessage,
+                $this->basepackages->progress->packagesData->responseCode
+            );
+        }
+    }
+}
